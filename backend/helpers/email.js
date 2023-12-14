4760ac4e31c49d068498641a5ae5f6af1f39d70c
jinhuanchen12 <jhchen@misena.edu.co>
2023-12-14 00:52:33 -0500

    hecho cambio

--- backend/helpers/email.js
+++ backend/helpers/email.js
@@ -17,11 +17,12 @@ export const emailRegistro = async (datos) => {
           const info = await transport.sendMail({
             from: '"UpTask - Comprueba tu Cuenta" <cuentas@uptask.com>',
             to: email,
+            subject: "Uptask - Compruba tu cuenta",
             text: "Comprueba tu cuenta en UpTask",
             html: `<p>Hola:${nombre} Compruba tu cuenta en UpTask</p>
-            <p>Tu cuneta ya esta casi lista, solo debes comprobarla en el siguiente enlace:
+            <p>Tu cuneta ya esta casi lista, solo debes comprobarla en el siguiente enlace:</p>
             <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar Cuenta</a>
-            </p>
+            
             <p>Si tu no creaste esta cuenta, puedes ignorar el mensaje</p>
 
             `,
