0cc940c0e4f4d879ca88914623d6a48906fb13a6
jinhuanchen12 <jhchen@misena.edu.co>
2024-03-06 01:48:00 -0500

    cambio el imagen

--- frontend/src/paginas/Emprendimiento.jsx
+++ frontend/src/paginas/Emprendimiento.jsx
@@ -24,29 +24,29 @@ const Emprendimiento = () => {
   if (cargando) return 'Cargando....';
 
   return (
-    <div className='max-w-2xl mx-auto mt-8'>
+    <div className='max-w-2xl mx-auto mt-8  '>
       <div className='bg-white rounded-lg shadow-md p-8'>
         <h1 className='font-black text-4xl mb-4'>{titulo}</h1>
 
         {imagen && (
           <div className='mb-4'>
-            <img src={imagen.url} alt={titulo} className='w-full h-auto rounded-md' />
+            <img src={imagen.url} alt={titulo} className='w-32 h-auto rounded-md' />
           </div>
         )}
 
-        <p className='text-gray-500 mb-2'>Teléfono: {telefono}</p>
-        <p className='text-gray-500 mb-2'>Dirección: {direccion}</p>
+        <p className='text mb-2'>Telefono: {telefono}</p>
+        <p className='text mb-2'>Dirección: {direccion}</p>
+        <p className='text-lg mb-2'>Descripcion: {descripcion}</p>
         <p className='text-lg mb-2'>Beneficiario: {beneficiario}</p>
         <p className='text-lg mb-2'>Presupuestos: {presupuestos}</p>
         {/* Agrega aquí otros campos que desees mostrar */}
         
-        <p className='text-lg mb-4'>{descripcion}</p>
+        
         
         <div className='flex justify-between items-center mt-4'>
           <Link
             to={`/emprendimientos/editar/${params.id}`}
-            className='uppercase font-bold'
-          >
+            className='uppercase font-bold'>
             Editar
           </Link>
           {/* Otros elementos o acciones que desees agregar */}
